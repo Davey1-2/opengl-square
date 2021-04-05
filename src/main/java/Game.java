bf3e613a126eb3f379a2996ea85486d23020d42a
Davey1-2 <david.barta2014gmail.com>
2021-04-05 14:53:31 +0200

    Problem

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,4 +1,6 @@
 import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL15;
 import org.lwjgl.opengl.GL33;
 import org.lwjgl.system.MemoryUtil;
 
@@ -7,15 +9,20 @@ import java.nio.FloatBuffer;
 public class Game {
 
     private static final float vertices[] = {
-            -0.5f, -0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            0.5f, 0.5f, 0.0f,
-            -0.5f, -0.5f, 0.0f,
-            -0.5f, 0.5f, 0.0f,
-            0.5f, 0.5f, 0.0f
+
+            0.5f,  0.5f, 0.0f,  // top right
+            0.5f, -0.5f, 0.0f,  // bottom right
+            -0.5f, -0.5f, 0.0f,  // bottom left
+            -0.5f,  0.5f, 0.0f   // top left
     };
+    private static final float indices[] = {  // note that we start from 0!
+            0, 1, 3,   // first triangle
+            1, 2, 3    // second triangle
+    };
+
     private static int triangleVaoId;
     private static int triangleVboId;
+    private static int EBO;
 
 
     public static void init(long window) {
@@ -26,6 +33,8 @@ public class Game {
         // Generate all the ids
         triangleVaoId = GL33.glGenVertexArrays();
         triangleVboId = GL33.glGenBuffers();
+        EBO = GL33.glGenBuffers();
+
 
         // Tell OpenGL we are currently using this object (vaoId)
         GL33.glBindVertexArray(triangleVaoId);
@@ -36,8 +45,16 @@ public class Game {
                 .put(vertices)
                 .flip();
 
+        FloatBuffer ind = BufferUtils.createFloatBuffer(indices.length)
+                .put(indices)
+                .flip();
+
         // Send the buffer (positions) to the GPU
         GL33.glBufferData(GL33.GL_ARRAY_BUFFER, fb, GL33.GL_STATIC_DRAW);
+
+        GL33.glBindBuffer(GL33.GL_ELEMENT_ARRAY_BUFFER, EBO);
+        GL33.glBufferData(GL33.GL_ELEMENT_ARRAY_BUFFER, ind,GL33.GL_STATIC_DRAW );
+
         GL33.glVertexAttribPointer(0, 3, GL33.GL_FLOAT, false, 0, 0);
         GL33.glEnableVertexAttribArray(0);
 
@@ -48,7 +65,8 @@ public class Game {
     public static void render(long window) {
         GL33.glUseProgram(Shaders.shaderProgramId);
         GL33.glBindVertexArray(triangleVaoId);
-        GL33.glDrawArrays(GL33.GL_TRIANGLES, 0, vertices.length / 3);
+        GL33.glDrawArrays(GL33.GL_TRIANGLES, 6, EBO);
+        GL33.glBindVertexArray(0);
     }
 
     public static void update(long window) {
